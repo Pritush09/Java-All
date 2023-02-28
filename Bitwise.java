b4fa32867242e646272669a39d32e6021bdee499
Pritush Singh <mynameispritush.15@gmail.com>
2023-02-28 15:14:13 +0530

    starting with the bit masking

---
+++ Bitwise.java
@@ -0,0 +1,10 @@
+public class Bitwise {
+    // changes we try to perform in a number is called bitmask
+    // there is different bitmask for every operation
+
+    public static void main(String[] args) {
+        int n = 5;
+        int pos = 2;
+        
+    }
+}
